1770d3a9f0d911567943725b310f298ed6abe9b4
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-07-27 14:35:40 +0500

    feat: add upload files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { checkedCountFiles } from './commons/checkedCountFiles';
 
 function App() {
+  const [files, setFiles] = useState<File[]>([]);
+
+  const onChangeFiles = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const selectedFiles = Array.from(event.target.files);
+
+      setFiles(checkedCountFiles(files, selectedFiles, 2));
+    }
+
+    event.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {files.map((el, index) => 
+          <section key={index}>{el.name}</section>
+        )}
+      </div>
+      <input type='file' multiple onChange={(event: ChangeEvent<HTMLInputElement>) => onChangeFiles(event)}/>
     </div>
   );
 }
