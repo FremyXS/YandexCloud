7d38ca6a95b72772fd978231201608db038b952a
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-07-28 22:41:52 +0500

    feat: add request

---
+++ src/commons/api/info/index.ts
@@ -0,0 +1,11 @@
+import { api } from ".."
+
+export const getInfo = async () => {
+    try{
+        const {data} = await api().get('');
+        return data;
+    }
+    catch{
+        return false;
+    }
+}
