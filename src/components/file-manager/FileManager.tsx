cb5c5414fd0422b2e24f6b90d7a5fd9610467721
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-07-29 00:54:00 +0500

    refactor: change component, add other components

--- src/components/file-manager/FileManager.tsx
+++ src/components/file-manager/FileManager.tsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import FileManagerHead from './components/FileManagerHead/FileManagerHead';
 import FileManagerBody from "./components/FileManagerBody/FileManagerBody";
+import FileManagerBottom from "./components/FileManagerBottom/FileManagerBottom";
+import { checkedCountFiles } from "../../commons/checkedCountFiles";
+import { uploadFiles } from "../../commons/uploadFiles";
 
 import './FileManager.scss';
 
-interface IFileManager {
-    files: File[]
-}
+function FileManager() {
+    const [files, setFiles] = useState<File[]>([])
 
-function FileManager({ files }: IFileManager) {
+    const onChangeFiles = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            const selectedFiles = Array.from(event.target.files);
+
+            setFiles(checkedCountFiles(files, selectedFiles, 100));
+        }
+
+        event.preventDefault();
+    }
+
+    const onUploadFiles = () => {
+        uploadFiles(files);
+    }
+
     return (
         <div className="file-manager">
-            <table>
-                <FileManagerHead />
-                <FileManagerBody files={files} />
-            </table>
+            <div className="file-manager-table">
+                <table>
+                    <FileManagerHead />
+                    <FileManagerBody files={files} />
+                </table>
+            </div>
+            <FileManagerBottom
+                onChangeFiles={(event: ChangeEvent<HTMLInputElement>) => onChangeFiles(event)}
+                onUploadFiles={onUploadFiles}
+                countFiles={files.length} />
         </div>
     )
 }
