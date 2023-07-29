ee7717c2a91b08986e2ab31052f91c474244cebe
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-07-29 19:25:33 +0500

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
-import React, { ChangeEvent, useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import FileManager from './components/file-manager/FileManager';
+import AuthWindow from './components/auth-mindow/AuthWindow';
 import './App.css';
-import { checkedCountFiles } from './commons/checkedCountFiles';
 
 function App() {
-  const [files, setFiles] = useState<File[]>([]);
-
-  const onChangeFiles = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      const selectedFiles = Array.from(event.target.files);
-
-      setFiles(checkedCountFiles(files, selectedFiles, 2));
-    }
-
-    event.preventDefault();
-  }
 
   return (
-    <div>
-      <div>
-        {files.map((el, index) => 
-          <section key={index}>{el.name}</section>
-        )}
-      </div>
-      <input type='file' multiple onChange={(event: ChangeEvent<HTMLInputElement>) => onChangeFiles(event)}/>
-    </div>
+    <div className='App'>
+      <AuthWindow />
+      <FileManager />
+    </div >
   );
 }
 
