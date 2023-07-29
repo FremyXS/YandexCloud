ec5188abe1c9fd9ac29bd6be6fd49911844205e7
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-07-29 21:26:17 +0500

    refactor: add api: check folder/file, create folder

--- src/commons/api/resources/index.ts
+++ src/commons/api/resources/index.ts
@@ -1,20 +1,41 @@
 import axios from "axios";
 import { api } from ".."
 
-export const upload = async (file: File) => {
+export const checkFolder = async (folderName: string) => {
+    try {
+        const {data} = await api().get(`resources?path=${folderName}`)
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+        return false;
+    }
+}
 
+export const createFolder = async (folderName: string) => {
+    try {
+        const {data} = await api().put(`resources?path=${folderName}`)
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+export const upload = async (folderName: string, file: File) => {
     try {
 
         const { data: downloadPath } = await api().get('resources/upload', {
             params: {
-                path: file.name,
+                path: `${folderName}/${file.name}`,
                 overwrite: false
             }
         })
 
         const fileData = new FormData();
         fileData.append('file', file);
-        
+
         const { data } = await axios.put(downloadPath.href, fileData);
         return data;
     }
